347a0b620ae821ea15c9f6ea49c1210af33f4b11
peterdesmet <peter.desmet@inbo.be>
2021-06-03 18:20:15 +0200

    Start read_descriptor function

---
+++ R/read_descriptor.R
@@ -0,0 +1,26 @@
+#' Read datapackage.json file
+#'
+#' Loads information from a `datapackage.json` file, i.e. the
+#' [Data Package descriptor](https://specs.frictionlessdata.io/data-package/#descriptor))
+#' file that describes the Data Package metadata and its resources.
+#'
+#' @param descriptor_file Path to `datapackage.json` file.
+#'
+#' @return Descriptor object.
+#'
+#' @export
+#'
+#' @importFrom jsonlite fromJSON
+#'
+#' @examples
+#' \dontrun{
+#' descriptor <- read_descriptor(system.file("extdata", "datapackage.json", package = "datapackage"))
+#' }
+read_descriptor <- function(descriptor_file) {
+  descriptor <- fromJSON(descriptor_file, simplifyDataFrame = FALSE)
+
+  # Add property resource_names
+  descriptor$resource_names = map_chr(descriptor$resources, "name")
+
+  descriptor
+}
