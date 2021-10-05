7d57b05e07137a2a3d9d975fd14e814aa42ee035
peterdesmet <peter.desmet@inbo.be>
2021-10-05 11:15:18 +0200

    Update check_path() function + add unsafe property

--- R/zzz.R
+++ R/zzz.R
@@ -4,20 +4,22 @@
 #'
 #' Check if a
 #' [URL or path](https://specs.frictionlessdata.io/data-resource/#url-or-path)
-#' is valid and prepend with directory to create a full path. Returns error for
-#' unsafe absolute and relative parent paths, or when no file can be found.
+#' is valid and prepend with directory to create a full path. Returns error when
+#' no file can be found and optionally for unsafe absolute and relative parent
+#' paths.
 #'
 #' @param url_or_path URL or path to a file.
 #' @param directory Directory to prepend to path.
+#' @param unsafe Allow unsafe absolute and relative parent paths.
 #'
 #' @return Full path or error.
 #'
-#' @internal
+#' @keywords internal
 #'
 #' @importFrom assertthat assert_that
 #' @importFrom glue glue
 #' @importFrom httr http_error
-check_path <- function(url_or_path, directory = ".") {
+check_path <- function(url_or_path, directory = NULL, unsafe = TRUE) {
   # url
   if (startsWith(url_or_path, "http")) {
     full_path <- url_or_path
@@ -28,16 +30,19 @@ check_path <- function(url_or_path, directory = ".") {
   # path
   } else {
     assert_that(
-      !startsWith(url_or_path, "/"),
+      unsafe | !startsWith(url_or_path, "/"),
       msg = glue("{url_or_path} is an absolute path (`/`) which is unsafe.")
     )
     assert_that(
-      !startsWith(url_or_path, "../"),
+      unsafe | !startsWith(url_or_path, "../"),
       msg = glue(
         "{url_or_path} is a relative parent path (`../`) which is unsafe."
       )
     )
-    full_path <- paste(directory, url_or_path, sep = "/")
+    # Prepend with directory if one is provided
+    full_path <- ifelse(
+      is.null(directory), url_or_path, paste(directory, url_or_path, sep = "/")
+    )
     assert_that(
       file.exists(full_path),
       msg = glue("Can't find file at `{full_path}`.")
