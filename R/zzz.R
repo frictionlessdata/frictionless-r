74ccffef2358ecd6763755bab85838bf074bf0fc
peterdesmet <peter.desmet@inbo.be>
2021-10-05 12:24:36 +0200

    Fix bug in check_path() and check path first, than existence of file
    Directory can be a URL as well, so first a full path needs to be build and only then can it be tested for a file

--- R/zzz.R
+++ R/zzz.R
@@ -25,53 +25,55 @@ unique_sorted <- function(x) {
     pull("Group.1")
 }
 
-#' Check URL or path
+#' Check path or URL
 #'
 #' Check if a
-#' [URL or path](https://specs.frictionlessdata.io/data-resource/#url-or-path)
-#' is valid and prepend with directory to create a full path. Returns error when
-#' no file can be found and optionally for unsafe absolute and relative parent
-#' paths.
+#' [path or URL](https://specs.frictionlessdata.io/data-resource/#url-or-path)
+#' is valid (and optionally safe) and prepend with directory to create an
+#' absolute path or URL. Returns error when no file can be found.
 #'
-#' @param url_or_path URL or path to a file.
+#' @param path Path or URL to a file.
 #' @param directory Directory to prepend to path.
-#' @param unsafe Allow unsafe absolute and relative parent paths.
+#' @param unsafe Allow `path` to be an unsafe absolute or relative parent path.
 #'
-#' @return Full path or error.
+#' @return Absolute parh or URL.
 #'
 #' @keywords internal
 #'
 #' @importFrom assertthat assert_that
 #' @importFrom glue glue
 #' @importFrom httr http_error
-check_path <- function(url_or_path, directory = NULL, unsafe = TRUE) {
-  # url
-  if (startsWith(url_or_path, "http")) {
-    full_path <- url_or_path
+check_path <- function(path, directory = NULL, unsafe = TRUE) {
+
+  # Check that (non-URL) path is safe and prepend with directory to make
+  # absolute path (both optional)
+  if (!startsWith(path, "http")) {
     assert_that(
-      !http_error(full_path),
-      msg = glue("Can't find file at `{full_path}`.")
-    )
-  # path
-  } else {
-    assert_that(
-      unsafe | !startsWith(url_or_path, "/"),
-      msg = glue("{url_or_path} is an absolute path (`/`) which is unsafe.")
+      unsafe | !startsWith(path, "/"),
+      msg = glue("{path} is an absolute path (`/`) which is unsafe.")
     )
     assert_that(
-      unsafe | !startsWith(url_or_path, "../"),
+      unsafe | !startsWith(path, "../"),
       msg = glue(
-        "{url_or_path} is a relative parent path (`../`) which is unsafe."
+        "{path} is a relative parent path (`../`) which is unsafe."
       )
     )
-    # Prepend with directory if one is provided
-    full_path <- ifelse(
-      is.null(directory), url_or_path, paste(directory, url_or_path, sep = "/")
-    )
+    if (!is.null(directory)) {
+      path <- paste(directory, path, sep = "/")
+    }
+  }
+
+  # Check existence of file at path
+  if (startsWith(path, "http")) {
     assert_that(
-      file.exists(full_path),
-      msg = glue("Can't find file at `{full_path}`.")
+      !http_error(path),
+      msg = glue("Can't find file at `{path}`.")
+    )
+  } else {
+    assert_that(
+      file.exists(path),
+      msg = glue("Can't find file at `{path}`.")
     )
   }
-  return(full_path)
+  return(path)
 }
