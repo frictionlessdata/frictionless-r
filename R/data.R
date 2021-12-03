dd036501150897d15035368a916b9ca915d6f42e
peterdesmet <peter.desmet@inbo.be>
2021-12-03 14:11:57 +0100

    Save example_package as data variable and document how it was created

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Example Data Package
+#'
+#' Example Tabular
+#' [Data Package](https://specs.frictionlessdata.io/data-package/) with dummy
+#' camera trap data organized in 3 Data Resources:
+#' - `deployments`: data stored in `deployments.csv`.
+#' - `observations`: data stored in `observations_1.csv` and
+#' `observations_2.csv`.
+#' - `media`: data stored in `data` property.
+#'
+#' @source <https://github.com/frictionlessdata/frictionless-r/tree/main/inst/extdata>
+#' @examples
+#' \dontrun{
+#' # File was created with
+#' example_package <- read_package(
+#'   system.file("extdata", "datapackage.json", package = "frictionless")
+#' )
+#' save(example_package, file = "data/example_package")
+#' }
+"example_package"
