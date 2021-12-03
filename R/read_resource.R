ce2ec126865dbb0f3c69b2ae7699f3733e079213
peterdesmet <peter.desmet@inbo.be>
2021-12-03 12:37:23 +0100

    Use as_tibble

--- R/read_resource.R
+++ R/read_resource.R
@@ -300,12 +300,12 @@ read_resource <- function(package, resource_name) {
 
   # Read data directly
   if (resource$read_from == "df") {
-    df <- dplyr::tibble(resource$data)
+    df <- dplyr::as_tibble(resource$data)
 
   # Read data from data
   } else if (resource$read_from == "data") {
     message("Reading data from `data` property.")
-    df <- dplyr::tibble(do.call(rbind.data.frame, resource$data))
+    df <- dplyr::as_tibble(do.call(rbind.data.frame, resource$data))
 
   # Read data from path(s)
   } else if (resource$read_from == "path") {
