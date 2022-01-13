01e383f81272ba32fe33ca933896e1e5a7a5f77f
peterdesmet <peter.desmet@inbo.be>
2022-01-13 16:54:27 +0100

    Set profile to "tabular-data-package" when creating package

--- tests/testthat/test-create_package.R
+++ tests/testthat/test-create_package.R
@@ -1,3 +1,8 @@
 test_that("create_package() returns a valid Data Package", {
   expect_true(check_package(create_package()))
 })
+
+test_that("create_package() sets profile to 'tabular-data-package'", {
+  p <- create_package()
+  expect_identical(p$profile, "tabular-data-package")
+})
