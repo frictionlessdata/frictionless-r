861d808d510e4aace8badd416c651895923480c0
Peter Desmet <peter.desmet@inbo.be>
2023-03-24 19:22:56 +0100

    Create rd file after all

--- R/utils-problems.R
+++ R/utils-problems.R
@@ -1,11 +1,13 @@
 #' Retrieve parsing problems
 #'
+#' Allows to show parsing issues in data frame returned by [read_resource()].
 #' See [readr::problems()] for details.
 #'
 #' @name problems
 #' @importFrom readr problems
 #' @export
-#' @noRd
-# This function is added to the NAMESPACE so users have immediate access to it
-# without having to load readr. Inspired by usethis::use_pipe()
+#' @keywords internal
+# This function is added to the NAMESPACE so users don't have to load readr.
+# Inspired by usethis::use_pipe()
+# Required to have .rd file, but not listed in function reference (internal).
 NULL
