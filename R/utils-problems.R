090a1fe0b3341032ea361a518c36a64d6883eca8
Peter Desmet <peter.desmet@inbo.be>
2023-03-24 17:50:57 +0100

    Don't create rd file for problems()
    This has the advantage that users will immediately see the readr help when doing ?problems()` rather than "Help on topic 'problems' was found in the following packages:"
    
    Ping @PietrH

--- R/utils-problems.R
+++ R/utils-problems.R
@@ -3,10 +3,9 @@
 #' See [readr::problems()] for details.
 #'
 #' @name problems
-#' @rdname problems
-#' @keywords internal
-#' @export
 #' @importFrom readr problems
+#' @export
+#' @noRd
 # This function is added to the NAMESPACE so users have immediate access to it
 # without having to load readr. Inspired by usethis::use_pipe()
 NULL
