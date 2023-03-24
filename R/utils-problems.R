89d75f9e4f3f377d32c75d01c0782ff6e315742d
Peter Desmet <peter.desmet@inbo.be>
2023-03-24 17:43:30 +0100

    Update problems() description + clarify in comment
    @PietrH See line 3: you can use markdown in description

--- R/utils-problems.R
+++ R/utils-problems.R
@@ -1,10 +1,12 @@
 #' Retrieve parsing problems
 #'
-#' See \code{readr::\link[readr:problems]{problems}} for details.
+#' See [readr::problems()] for details.
 #'
 #' @name problems
 #' @rdname problems
 #' @keywords internal
 #' @export
 #' @importFrom readr problems
+# This function is added to the NAMESPACE so users have immediate access to it
+# without having to load readr. Inspired by usethis::use_pipe()
 NULL
