d790f96da5277dbb4d0fc4f9999438c7ac515fa0
Peter Desmet <peter.desmet@inbo.be>
2023-03-27 10:03:34 +0200

    Repharse

--- R/utils-problems.R
+++ R/utils-problems.R
@@ -1,6 +1,6 @@
 #' Retrieve parsing problems
 #'
-#' Allows to show parsing issues in data frame returned by [read_resource()].
+#' Retrieves the parsing problems in data frame returned by [read_resource()].
 #' See [readr::problems()] for details.
 #'
 #' @name problems
@@ -9,5 +9,5 @@
 #' @keywords internal
 # This function is added to the NAMESPACE so users don't have to load readr.
 # Inspired by usethis::use_pipe()
-# Required to have .rd file, but not listed in function reference (internal).
+# Required to have an .rd file, but not listed in function reference (internal).
 NULL
