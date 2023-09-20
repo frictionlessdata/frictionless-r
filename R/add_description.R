968670b61ea4d0bc3e1fad89dde8d45f69596bfc
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-09-19 20:07:46 -0700

    ✅ Pass test: `get_field_names()`

--- R/add_description.R
+++ R/add_description.R
@@ -9,3 +9,8 @@ add_description <- function(old_schema, descriptions) {
 
 
 edit_fields <- function(old_schema, metadata_name, metadata) {}
+
+
+get_field_names <- function(old_schema) {
+  comprehenr::to_vec(for (field in old_schema$fields) field$name)
+}
