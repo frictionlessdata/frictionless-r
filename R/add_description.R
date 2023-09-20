03d043995d1ca4b0af03766dcacaf439d429ee19
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-09-19 20:31:22 -0700

    🚧 Add empty function

--- R/add_description.R
+++ R/add_description.R
@@ -11,6 +11,9 @@ add_description <- function(old_schema, descriptions) {
 edit_fields <- function(old_schema, metadata_name, metadata) {}
 
 
+edit_field_from_name <- function(old_schema, field_name, metadata_name, metadata) {}
+
+
 get_field_names <- function(old_schema) {
   comprehenr::to_vec(for (field in old_schema$fields) field$name)
 }
