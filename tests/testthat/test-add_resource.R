5c1fde2c782b431952d2724f3c06b3e5491505c2
Peter Desmet <peter.desmet@inbo.be>
2023-12-26 18:13:54 +0100

    Use CLI and error classes for check_package()

--- tests/testthat/test-add_resource.R
+++ tests/testthat/test-add_resource.R
@@ -15,11 +15,7 @@ test_that("add_resource() returns error on incorrect Data Package", {
   df <- data.frame("col_1" = c(1, 2), "col_2" = c("a", "b"))
   expect_error(
     add_resource(list(), "new", df),
-    paste(
-      "`package` must be a list describing a Data Package,",
-      "created with `read_package()` or `create_package()`."
-    ),
-    fixed = TRUE
+    class = "frictionless_error_package_incorrect"
   )
 })
 
