b0a29ad6221571fced204224c28c2f61ee03c8fb
Peter Desmet <peter.desmet@inbo.be>
2024-03-14 11:42:12 +0100

    Use cli_inform() for downloading file

--- tests/testthat/test-write_package.R
+++ tests/testthat/test-write_package.R
@@ -253,12 +253,18 @@ test_that("write_package() shows message when downloading file", {
   skip_if_offline()
   p <- example_package
   dir <- file.path(tempdir(), "package")
+  dir_1 <- file.path(dir, "1")
+  dir_2 <- file.path(dir, "2")
   on.exit(unlink(dir, recursive = TRUE))
   expect_message(
-    write_package(p, dir),
-    paste0(
-      "Downloading file from https://raw.githubusercontent.com/",
-      "frictionlessdata/frictionless-r/main/inst/extdata/deployments.csv"
+    write_package(p, dir_1),
+    class = "frictionless_message_file_downloading"
+  )
+  expect_message(
+    write_package(p, dir_2),
+    regexp = paste0(
+      "Downloading file from 'https://raw.githubusercontent.com/",
+      "frictionlessdata/frictionless-r/main/inst/extdata/deployments.csv'"
     ),
     fixed = TRUE
   )
