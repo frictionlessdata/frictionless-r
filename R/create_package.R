6bd16c6d4b626423e917cde5cc387e4c23685f38
Peter Desmet <peter.desmet@inbo.be>
2024-03-19 16:52:54 +0100

    Allow to provide a descriptor to create_package() and document
    - Don't double set `datapackage`
    - Return descriptor

--- R/create_package.R
+++ R/create_package.R
@@ -1,28 +1,49 @@
-#' Create an empty Data Package
+#' Create a Data Package
 #'
-#' Initiates a [Data Package]
-#' (https://specs.frictionlessdata.io/data-package/), a list with a
-#' `datapackage` class.
-#' This empty Data Package can be extended with metadata and resources (see
-#' [add_resource()]).
-#' Added resources will make the Data Package meet [Tabular Data
-#' Package](https://specs.frictionlessdata.io/tabular-data-package/)
-#' requirements, so `profile` is set to `tabular-data-package`.
+#' Initiates a [Data Package](https://specs.frictionlessdata.io/data-package/)
+#' object, either from scratch or from an existing list.
+#' This Data Package object is a list with a `datapackage` class and the
+#' following properties:
+#' - All properties of the original `descriptor`.
+#' - [`resources`](
+#'   https://specs.frictionlessdata.io/data-package/#required-properties), if
+#'   not provided.
+#' - [`profile`](https://specs.frictionlessdata.io/data-package/#profile), if
+#'   not provided.
+#'   This property is set to `"tabular-data-package"`, since added resources
+#'   will make the Data Package meet [Tabular Data Package](
+#'   https://specs.frictionlessdata.io/tabular-data-package/) requirements.
+#' - `directory`, if not provided.
+#'   It is used as the base path to read resources with [read_resource()], with
+#'   the current directory (`"."`) by default.
 #'
+#' @param descriptor List to be made into a Data Package object.
+#'   If `NULL`, an empty Data Package object will be created from scratch.
 #' @return Data Package object.
+#'   Use [check_package()] to check if the output is a valid Data Package
+#'   object.
 #' @family create functions
 #' @export
 #' @examples
 #' # Create a Data Package
 #' package <- create_package()
 #' str(package)
-create_package <- function() {
-  descriptor <- list(
-    profile = "tabular-data-package",
-    resources = list(),
-    directory = "." # Current directory
-  )
+create_package <- function(descriptor = NULL) {
+  if (!is.null(descriptor) && !is.list(descriptor)) {
+    cli::cli_abort(
+      "{.arg x} must be a list.",
+      class = "frictionless_error_descriptor_invalid"
+    )
+  }
+
+  # Add properties
+  descriptor$profile <- replace_null(descriptor$profile, "tabular-data-package")
+  descriptor$resources <- replace_null(descriptor$resources, list())
+  descriptor$directory <- replace_null(descriptor$directory, ".") # Current dir
 
   # Add datapackage class
-  class(descriptor) <- c("datapackage", class(descriptor))
+  if (!"datapackage" %in% class(descriptor)) {
+    class(descriptor) <- c("datapackage", class(descriptor))
+  }
+  descriptor
 }
--- tests/testthat/test-create_package.R
+++ tests/testthat/test-create_package.R
@@ -1,12 +1,35 @@
-test_that("create_package() returns a valid Data Package", {
+test_that("create_package() initiates a valid data package", {
   expect_true(check_package(create_package()))
 })
 
-test_that("create_package() sets profile to 'tabular-data-package'", {
-  p <- create_package()
-  expect_identical(p$profile, "tabular-data-package")
+test_that("create_package() sets profile (to 'tabular-data-package')", {
+  new <- create_package()
+  expect_identical(new$profile, "tabular-data-package")
+
+  existing <- create_package(list(profile = "not_default"))
+  expect_identical(existing$profile, "not_default")
+})
+
+test_that("create_package() sets resources", {
+  new <- create_package()
+  expect_identical(new$resources, list())
+
+  existing <- create_package(list(resources = "not_default"))
+  expect_identical(existing$resources, "not_default")
+})
+
+test_that("create_package() sets directory", {
+  new <- create_package()
+  expect_identical(new$directory, ".")
+
+  existing <- create_package(list(directory = "not_default"))
+  expect_identical(existing$directory, "not_default")
 })
 
 test_that("create_package() adds class 'datapackage'", {
-  expect_s3_class(create_package(), "datapackage")
+  new <- create_package()
+  expect_s3_class(new, "datapackage")
+
+  existing <- create_package(list())
+  expect_s3_class(new, "datapackage")
 })
