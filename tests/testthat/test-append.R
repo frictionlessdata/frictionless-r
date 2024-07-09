d69471d17c088d8933a55b1b97af6e4236e52832
PietrH <pieterhuy@gmail.com>
2024-07-09 10:25:53 +0200

    Write test for custom append method for `datapackage` objects

---
+++ tests/testthat/test-append.R
@@ -0,0 +1,11 @@
+test_that("datapackages can be appended without losing their class", {
+  # Create a datapackage to append
+  datapackage_to_append <- create_package()
+  # Append with a value
+  datapackage_appended <- append(datapackage_to_append, c(ten = "forward"))
+  # Check for class
+  expect_s3_class(
+    append(datapackage_to_append, c(ten = "forward")),
+    "datapackage"
+  )
+})
