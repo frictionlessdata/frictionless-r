45c45801128868aff0a8c2969db30cf83714f710
PietrH <pieterhuy@gmail.com>
2024-07-09 11:19:27 +0200

    Create append method for datapackages

---
+++ R/append.R
@@ -0,0 +1,30 @@
+#' Custom method for base append to retain custom S3 classes
+#'
+#' When base::append is used on a datapackage object, it loses it's custom s3
+#' class. This is a custom method, that retains this s3 class.
+#'
+#' @inheritParams base::append
+#'
+#' @return Data Package
+#' @method append datapackage
+#' @export
+#'
+#' @examples
+#' p <- create_package()
+#' class(p)
+#' p_appended <- append(p, c(new_record_to_append = "value to append"))
+#' class(p_appended)
+append.datapackage <- function(x, values, after = length(x)){
+  # First store the class of the datapackage object, store it explicitly in case
+  # it has more attributes than just list and datapackage.
+  datapackage_class <- class(x)
+
+  # Append the object
+  datapackage_appended <- append(x, values, after = after)
+
+  # Reset the class back to how we found it
+  class(datapackage_appended) <- datapackage_class
+
+  # Return the datapackage with the correct class
+  return(datapackage_appended)
+}
