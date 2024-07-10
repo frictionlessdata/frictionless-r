5dc65c237034f82be8ba0360019f6c5da4b0cd0e
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-07-10 17:16:52 +0200

    Move and improve get_encoding()

--- R/utils.R
+++ R/utils.R
@@ -77,30 +77,6 @@ read_descriptor <- function(x, directory = NULL, safe = FALSE) {
   }
 }
 
-#' Read encoding
-#'
-#' Returns the encoding of a [Data
-#' Resource](https://specs.frictionlessdata.io/data-resource/) (in a Data
-#' Package), or sets it to UTF-8 if unknown.
-#' A warning is returned if the given encoding is unknown.
-#'
-#' @param resource Resource.
-#' @return Encoding of resource, or UTF-8 if unknown.
-#' @family helper functions
-#' @noRd
-get_encoding <- function(resource) {
-  encoding <- resource$encoding %||% "UTF-8" # Set default to UTF-8
-  if (!tolower(encoding) %in% tolower(iconvlist())) {
-    cli::cli_warn(
-      "Unknown encoding {.field {encoding}}. Reading file(s) with UTF-8
-         encoding.",
-      class = "frictionless_warning_resource_encoding_unknown"
-    )
-    encoding <- "UTF-8"
-  }
-  return(encoding)
-}
-
 #' Create locale for reading a Data Resource
 #'
 #' Create a [readr::locale()] object for reading a [Data
