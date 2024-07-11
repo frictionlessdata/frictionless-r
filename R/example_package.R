c2a2118c4ec5cb86736a8cf224bb30d7665c3882
Peter Desmet <peter.desmet@inbo.be>
2024-07-11 12:30:57 +0200

    Make observations a local package, so whole example_package() is local

--- R/example_package.R
+++ R/example_package.R
@@ -3,11 +3,10 @@
 #' Reads the example Data Package included in `frictionless`.
 #' This dataset is used in examples, vignettes, and tests and contains dummy
 #' camera trap data organized in 3 Data Resources:
-#' 1. `deployments`: local data file referenced in
+#' 1. `deployments`: one local data file referenced in
 #'    `"path": "deployments.csv"`.
-#' 2. `observations`: two remote data files referenced in
-#'    `"path": ["url/to/observations_1.csv", "url/to/observations_2.csv"]`.
-#'    This resource requires an internet connection to be read.
+#' 2. `observations`: two local data files referenced in
+#'    `"path": ["observations_1.csv", "observations_2.csv"]`.
 #' 3. `media`: inline data stored in `data`.
 #'
 #' @return Data Package object, see [create_package()].
