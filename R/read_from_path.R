732fcfb1b11710903be328060cc4524a9e9aa4c8
Peter Desmet <peter.desmet@inbo.be>
2024-07-17 14:15:10 +0200

    Update doc for read_from_path()

--- R/read_from_path.R
+++ R/read_from_path.R
@@ -1,58 +1,17 @@
-#' Read data from a path with user defined specifications.
+#' Read data from the path of a Data Resource
 #'
-#' @param x Path to file.
+#' @param path Path to file.
 #' @param dialect Dialect of the data in `path`.
 #' @param field_names Names of the schema's fields.
-#' @param col_types Column types, e.g. as created by `create_col_types()`.
+#' @param col_types Column types, e.g. as created by `cols()`.
 #' @param col_select Columns in data to read.
 #' @param schema Schema of the resource.
-#' @param locale Locale, e.g. as created by `create_locale()`.
-#' @return Data frame.
-#' @family helper functions
+#' @param locale Locale, e.g. as created by `locale()`.
+#' @return [tibble::tibble()] data frame
+#' @family read functions
 #' @noRd
-#' @examples
-#' # Load the example Data Package
-#' package <- example_package()
-#'
-#' # Get the path to a Resource
-#' path <- frictionless:::get_resource(package, "observations")$path[1]
-#' dialect <- NULL
-#' schema <- get_schema(package, "observations")
-#' # From https://github.com/frictionlessdata/frictionless-r/pull/237/files
-#' # To be replaced with get_fields_names().
-#' fields <- purrr::chuck(schema, "fields")
-#' field_names <- purrr::map_chr(fields, ~ purrr::pluck(.x, "name"))
-#' col_types <- frictionless:::create_col_types(package, "observations")
-#' col_select <- NULL
-#' locale <- frictionless:::create_locale(package, "observations")
-#' frictionless:::read_from_path(
-#'   path,
-#'   dialect,
-#'   field_names,
-#'   col_types,
-#'   col_select,
-#'   schema,
-#'   locale
-#' )
-#'
-#' # Select only some columns
-#' colselect <- c("observation_id", "deployment_id")
-#' frictionless:::read_from_path(
-#'   path,
-#'   dialect,
-#'   field_names,
-#'   col_types,
-#'   col_select,
-#'   schema,
-#'   locale
-#' )
-read_from_path <- function(x,
-                           dialect,
-                           field_names,
-                           col_types,
-                           col_select,
-                           schema,
-                           locale) {
+read_from_path <- function(path, dialect, field_names, col_types, col_select,
+                           schema, locale) {
   escape_backslash <- if (dialect$escapeChar %||% "not set" == "\\") {
     TRUE
   } else {
@@ -65,9 +24,9 @@ read_from_path <- function(x,
     dialect$doubleQuote %||% TRUE
   }
   skip <- if (dialect$header %||% TRUE) 1 else 0
-  col_select <- col_select
+  col_select <- col_select # Assign to avoid lazy evaluation
   readr::read_delim(
-    file = x,
+    file = path,
     delim = dialect$delimiter %||% ",",
     quote = dialect$quoteChar %||% "\"",
     escape_backslash = escape_backslash,
