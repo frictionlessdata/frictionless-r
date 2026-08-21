4028434307836ef7e6b5109b09f5152bb1b780ce
Peter Desmet <600993+peterdesmet@users.noreply.github.com>
2026-08-21 14:58:06 +0200

    Document and test support for generic version()

--- R/version.R
+++ R/version.R
@@ -1,22 +1,38 @@
 #' Get Data Package version
 #'
 #' Determines what version of the [Data Package standard](
-#' https://datapackage.org/) is used by a Data Package, based on the
-#' [`$schema`](https://datapackage.org/standard/data-package/#dollar-schema)
-#' property.
+#' https://datapackage.org/) is used by a Data Package, Data Resource, Table
+#' Dialect or Table Schema, based on the [`$schema`](
+#' https://datapackage.org/standard/data-package/#dollar-schema) property.
 #' Version `"1.0"` is assumed if `$schema` is missing, version `">=2.0"` is
 #' assumed for custom values (e.g. [extensions](
 #' https://datapackage.org/standard/extensions/)).
 #'
-#' @inheritParams read_resource
-#' @returns Data Package version number (e.g. `"1.0"`).
+#' @param x A list describing either a:
+#' - Data Package.
+#' - Data Resource.
+#' - Table Dialect.
+#' - Table Schema.
+#' @returns Data Package standard version number (e.g. `"1.0"`).
 #' @family version functions
 #' @export
 #' @examples
+#' # Data Package
 #' package <- example_package()
 #' version(package)
-version <- function(package) {
-  dollar_schema <- purrr::pluck(package, "$schema")
+#'
+#' # Data Resource
+#' resource <- resource(package, "observations")
+#' version(resource)
+#'
+#' # Table Dialect
+#' version(resource$dialect)
+#'
+#' # Table Schema
+#' schema <- schema(package, "observations")
+#' version(schema)
+version <- function(x) {
+  dollar_schema <- purrr::pluck(x, "$schema")
 
   if (is.null(dollar_schema)) {
     return("1.0") # Assume 1.0 if $schema is undefined
@@ -26,7 +42,8 @@ version <- function(package) {
   }
 
   # Extract version from e.g.
-  # "https://datapackage.org/profiles/<version>/datapackage.json"
+  # "https://datapackage.org/profiles/1.0/datapackage.json" or
+  # "https://datapackage.org/profiles/2.0/tableschema.json"
   pattern = "^https://datapackage\\.org/profiles/((?:[0-9A-Za-z]|\\.|-)+)/.*"
   extracted_version <- sub(
     pattern,
