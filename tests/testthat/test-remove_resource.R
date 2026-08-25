f94d84a9796cc5b0f8ca1008fcc57cebab96ed91
Peter Desmet <600993+peterdesmet@users.noreply.github.com>
2026-08-25 11:07:43 +0200

    Move test up + fix typo

--- tests/testthat/test-remove_resource.R
+++ tests/testthat/test-remove_resource.R
@@ -3,6 +3,13 @@ test_that("remove_resource() returns a valid Data Package", {
   expect_no_error(check_package(remove_resource(p, "deployments")))
 })
 
+test_that("remove_resource() returns package in same version as provided", {
+  p_v1 <- example_package(version = "1.0")
+  p_v2 <- example_package(version = "2.0")
+  expect_identical(version(remove_resource(p_v1, "deployments")), "1.0")
+  expect_identical(version(remove_resource(p_v2, "deployments")), "2.0")
+})
+
 test_that("remove_resource() returns error on invalid Data Package", {
   expect_error(
     remove_resource(list(), "deployments"),
@@ -39,11 +46,4 @@ test_that("remove_resource() removes resource", {
   # Resource removed
   expect_length(p_removed$resources, 2) # Remains a list, now of length 2
   expect_identical(p_removed$resources[[1]][["name"]], "observations")
-})
-
-test_that("remove_resource() returns package in same version as provided", {
-  p_v1 <- example_package(version = "1.0")
-  p_v2 <- example_package(version = "2.0")
-  expect_identical(version(remove_resource(p_1, "deployments")), "1.0")
-  expect_identical(version(remove_resource(p_2, "deployments")), "2.0")
 })
