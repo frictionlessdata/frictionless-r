9d390620c44478cc23ca26133dace2f129effd1d
Peter Desmet <600993+peterdesmet@users.noreply.github.com>
2026-08-25 11:14:18 +0200

    Remove explicit versions where not needed

--- tests/testthat/test-remove_resource.R
+++ tests/testthat/test-remove_resource.R
@@ -1,5 +1,5 @@
 test_that("remove_resource() returns a valid Data Package", {
-  p <- example_package(version = "2.0")
+  p <- example_package()
   expect_no_error(check_package(remove_resource(p, "deployments")))
 })
 
