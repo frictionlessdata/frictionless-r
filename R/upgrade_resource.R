ad4ea3201e02faac2dd867c310b9d1e9a1ea534e
Peter Desmet <600993+peterdesmet@users.noreply.github.com>
2026-08-25 17:50:35 +0200

    Move helpers to own files
    Just like `check_` functions they will be called from different functions and merit their own tests

---
+++ R/upgrade_resource.R
@@ -0,0 +1,33 @@
+#' Upgrade a Data Resource object from v1 to v2
+#'
+#' @param resource List describing a Data Resource, as returned by [resource()].
+#' @returns Upgraded `resource`.
+#' @family upgrade functions
+#' @noRd
+upgrade_resource <- function(resource) {
+  version <- version(resource)
+
+  # Leave resource as is if already 2.0 or higher
+  if (version != "1.0") {
+    return(resource)
+  }
+
+  # Set $schema
+  purrr::pluck(resource, "$schema") <-
+    "https://datapackage.org/profiles/2.0/dataresource.json"
+
+  # Set type to table if resource is tabular, see
+  # https://datapackage.org/standard/data-resource/#type
+  profile <- resource$profile %||% "undefined"
+  if (profile %in% c(
+    "tabular-data-resource",
+    "https://specs.frictionlessdata.io/schemas/tabular-data-resource.json"
+  )) {
+    resource$type <- "table"
+  }
+
+  # Remove profile, including URL to custom *resource* profile, but hardly used
+  resource$profile <- NULL
+
+  return(resource)
+}
