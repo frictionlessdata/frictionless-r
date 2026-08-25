10897457e60c8e619650ce8e8fa1e773a03c2fee
Peter Desmet <600993+peterdesmet@users.noreply.github.com>
2026-08-25 15:58:26 +0200

    Start upgrade_descriptor

--- R/utils-upgrade.R
+++ R/utils-upgrade.R
@@ -1,5 +1,44 @@
 # UPGRADE HELPER FUNCTIONS
 
+#' Upgrade a package object from v1 to v2
+#'
+#' Upgrades the descriptor, not the associated resources, etc.
+#'
+#' @inheritParams read_resource
+#' @returns Upgraded `package`.
+#' @family upgrade functions
+#' @noRd
+upgrade_descriptor <- function(package) {
+  version <- version(package)
+
+  # Leave descriptor as is if already 2.0 or higher
+  if (version != "1.0") {
+    return(package)
+  }
+
+  # Set $schema
+  purrr::pluck(package, "$schema") <-
+    "https://datapackage.org/profiles/2.0/datapackage.json"
+
+  # Set $schema to profile if URL (to custom profile)
+  profile <- package$profile %||% "undefined"
+  if (is_url(profile)) {
+    purrr::pluck(package, "$schema") <- profile
+  }
+
+  # Set $schema profile if "fiscal-data-package" and set to its URL
+  if (profile == "fiscal-data-package") {
+    purrr::pluck(package, "$schema") <-
+      "https://specs.frictionlessdata.io/schemas/fiscal-data-package.json"
+  }
+
+  # Remove profile
+  resource$profile <- NULL
+
+  # Update contributor roles
+  # TODO
+}
+
 #' Upgrade a Data Resource object from v1 to v2
 #'
 #' @param resource List describing a Data Resource, as returned by [resource()].
@@ -28,8 +67,7 @@ upgrade_resource <- function(resource) {
     resource$type <- "table"
   }
 
-  # Remove profile (https://specs.frictionlessdata.io/data-resource/#profile)
-  # This may remove URL to custom resource profile, but is seldom-used feature
+  # Remove profile, including URL to custom *resource* profile, but hardly used
   resource$profile <- NULL
 
   return(resource)
