76cdf5cc808e8c5f602608e7d2b143464d334096
Damiano Oldoni <damiano.oldoni@inbo.be>
2026-08-26 16:58:06 +0200

    Mention integer as fields with groupChar

--- R/locale.R
+++ R/locale.R
@@ -37,8 +37,8 @@ locale <- function(package, resource_name) {
   } else {
     grouping_mark <- g_chars[1]
     cli::cli_warn(
-       fields with {.val {g_chars[1]}} as grouping mark.",
       "Some fields define a {.field groupChar}. Parsing all number
+       and integer fields with {.val {g_chars[1]}} as grouping mark.",
       class = "frictionless_warning_fields_groupchar_different"
     )
   }
